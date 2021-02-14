b41595e322fd15b9e2d65576e715dbdd1d42d789
USER\namhd <namhd@tcbs.com.vn>
2021-02-14 09:55:52 +0700

    CRUD Operation

--- springDataJpa/src/main/java/com/namhd/springDataJpa/SpringDataJpaApplication.java
+++ springDataJpa/src/main/java/com/namhd/springDataJpa/SpringDataJpaApplication.java
@@ -3,14 +3,17 @@ package com.namhd.springDataJpa;
 import com.namhd.entity.Book;
 import com.namhd.repository.BookRepository;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 
 @SpringBootApplication
@@ -25,8 +28,20 @@ public class SpringDataJpaApplication {
 		book.setPageCount(200);
 		book.setPrice(new BigDecimal("100.00"));
 		book.setPublishDate(new Date());
+		book.setId(4);
+		repository.save(book); //update query because book id 4 already exists.
 
-		repository.save(book);
+		Book book1 = new Book();
+		book1.setTitle("The Manager's Path");
+		book1.setPageCount(300);
+		book1.setPrice(new BigDecimal("200.00"));
+		book1.setPublishDate(new Date());
+		repository.save(book1); //insert query because book id is not set or not found.
+
+		ArrayList<Long> ids = new ArrayList<>();
+		ids.add(1L);
+		ids.add(2L);
+		repository.deleteInBatch(repository.findAllById(ids));
 
 		System.out.println("=======Book list: " + repository.findAll());
 	}
---
+++ springDataJpa/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into book (id, title, page_count,price, publish_date) values (1, 'Factfullness', 200, 100.1, '1954-11-08');
+insert into book (id, title, page_count,price, publish_date) values (2, 'Factfullness1', 300, 200.1, '1954-11-08');
+insert into book (id, title, page_count,price, publish_date) values (3, 'Factfullness2', 400, 300.1, '1954-11-08');
+insert into book (id, title, page_count,price, publish_date) values (4, 'Factfullness3', 500, 400.1, '1954-11-08');
